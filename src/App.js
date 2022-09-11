83d8aab3f080d02a49b1b411ee745861e391381e
Jia <pochialu@gmail.com>
2022-09-11 22:54:18 +0800

    使用 useEffect() hook 在 component rendered 只執行一次變數判斷

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,28 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  /**
+   * useEffect 第二個參數 [] 為空時, 只在 component 第一次 rendered 時執行一次
+   * 藉此來查找 localStorage
+   */
+  useEffect(() => {
+    const storedUserLoginInfo = localStorage.getItem('isLoggedIn')
+
+    if (storedUserLoginInfo === '1') {
+      setIsLoggedIn(true)
+    }
+
+  }, [])
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1')
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
     setIsLoggedIn(false);
   };
 
