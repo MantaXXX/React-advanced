54e230f3fe5b047414c903d3406fa1eba2f1ec39
Jia <pochialu@gmail.com>
2022-09-18 23:29:19 +0800

    使用 useContext 簡化 AuthContext.Comsumer 來提升 JSX 的可讀性

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,38 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../store/AuthContext';
 
 const Navigation = (props) => {
+  // 使用 useContext 來接引用的 AuthContext，並將參數傳至 JSX 使用
+  const ctx = useContext(AuthContext)
   /**
    * 要使用 <AuthContext> 內的 state，需要在 JSX 使用 .Consumer
    * 且包含一個有 (ctx) => {} 在內的 {}
    * {(ctx) => {}}，ctx 為 createContext 所傳的參數
    */
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return <nav className={classes.nav}>
-          <ul>
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Users</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Admin</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <button onClick={props.onLogout}>Logout</button>
-              </li>
-            )}
-          </ul>
-        </nav>
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav} >
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
