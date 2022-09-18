e6c0b8e1c25ada64d1356ddb115f3778932d50ee
Jia <pochialu@gmail.com>
2022-09-18 23:20:15 +0800

    新增 Context 將 state 提升至全局使用，改寫 isLoggIn 狀態

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/AuthContext';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -21,8 +22,6 @@ function App() {
   }, [])
 
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
     localStorage.setItem('isLoggedIn', '1')
     setIsLoggedIn(true);
   };
@@ -31,15 +30,21 @@ function App() {
     localStorage.removeItem('isLoggedIn')
     setIsLoggedIn(false);
   };
-
+  /**
+   * <AuthContext> 並非 component，需用 .Provider 作為 component 才能用在 JSX 中
+   * <AuthContext.Provider> 內的 components 才能取得 context state
+   */
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn
+      }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,28 +1,38 @@
 import React from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContext from '../../store/AuthContext';
 
 const Navigation = (props) => {
+  /**
+   * 要使用 <AuthContext> 內的 state，需要在 JSX 使用 .Consumer
+   * 且包含一個有 (ctx) => {} 在內的 {}
+   * {(ctx) => {}}，ctx 為 createContext 所傳的參數
+   */
   return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <AuthContext.Consumer>
+      {(ctx) => {
+        return <nav className={classes.nav}>
+          <ul>
+            {ctx.isLoggedIn && (
+              <li>
+                <a href="/">Users</a>
+              </li>
+            )}
+            {ctx.isLoggedIn && (
+              <li>
+                <a href="/">Admin</a>
+              </li>
+            )}
+            {ctx.isLoggedIn && (
+              <li>
+                <button onClick={props.onLogout}>Logout</button>
+              </li>
+            )}
+          </ul>
+        </nav>
+      }}
+    </AuthContext.Consumer>
   );
 };
 
